cebf8883cd123afe21f905bbb2fe776ddf7e336c
Laura <laura.nagamine@utec.edu.pe>
2024-08-28 14:48:59 -0500

    forward list file

---
+++ ForwardList/ForwardList.h
@@ -0,0 +1,17 @@
+//
+// Created by laura on 27/08/2024.
+//
+
+#ifndef ESTRUCTURAS_DE_DATOS_LAURA_FORWARD_LIST_H
+#define ESTRUCTURAS_DE_DATOS_LAURA_FORWARD_LIST_H
+
+
+class ForwardList {
+private:
+    node;
+public:
+
+};
+
+
+#endif //ESTRUCTURAS_DE_DATOS_LAURA_FORWARD_LIST_H
